b7f18e3e208225e0e62689771f3ac051ed1c65bb
Taksh <taksh.malabiya@novicbase.com>
2025-07-10 10:45:27 +0530

    final commit

--- lib/screen/view/Register/bloc/register_bloc.dart
+++ lib/screen/view/Register/bloc/register_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
 
 part 'register_event.dart';
